20102f710dbf53ff9a6b6957bdd50ec7b0d07384
Victor Oseji <vctroseji@gmail.com>
2026-03-17 13:30:19 +0100

    minor updates

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -23,7 +23,7 @@ class PhotoController extends Controller
 
         $photos = $query
             ->orderBy('sort_order')
-            ->orderByAsc('created_at')
+            ->orderBy('created_at', 'desc')
             ->get([
                 'id',
                 'title',
